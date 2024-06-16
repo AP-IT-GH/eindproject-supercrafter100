c78ce41d5676f88b2a3c3dc02a8ed9175c3f10c8
Eline Van Zeir <elinevanzeir@hotmail.com>
2024-06-16 10:25:56 +0200

    extra

--- VR Room/Assets/Scripts/testAgent.cs
+++ VR Room/Assets/Scripts/testAgent.cs
@@ -7,51 +7,60 @@ using Unity.MLAgents.Actuators;
 using static UnityEngine.GraphicsBuffer;
 
 public class testAgent : Agent
-{ 
-public Transform Target;
-public float speedMultiplier = 0.1f;
-public override void OnEpisodeBegin()
 {
-    // reset de positie en orientatie als de agent gevallen is
-    if (this.transform.localPosition.y < 0)
+    public Transform Target;
+    public float speedMultiplier = 0.1f;
+    public float rotationMultiplier = 5;
+    public override void OnEpisodeBegin()
     {
+        // reset de positie en orientatie als de agent gevallen is
+        if (this.transform.localPosition.y < 0)
+        {
 
-        this.transform.localPosition = new Vector3(0, 0.5f, 0); this.transform.localRotation = Quaternion.identity;
+            this.transform.localPosition = new Vector3(0, 0.5f, 0); this.transform.localRotation = Quaternion.identity;
+        }
+
+        // verplaats de target naar een nieuwe willekeurige locatie 
+        Target.localPosition = new Vector3(Random.value * 8 - 4, 0.5f, Random.value * 8 - 4);
+    }
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        // Target en Agent posities
+        sensor.AddObservation(Target.localPosition);
+        sensor.AddObservation(this.transform.localPosition);
+
     }
 
-    // verplaats de target naar een nieuwe willekeurige locatie 
-    Target.localPosition = new Vector3(Random.value * 8 - 4, 0.5f, Random.value * 8 - 4);
-}
-public override void CollectObservations(VectorSensor sensor)
-{
-    // Target en Agent posities
-    sensor.AddObservation(Target.localPosition);
-    sensor.AddObservation(this.transform.localPosition);
-
-}
-
-public override void OnActionReceived(ActionBuffers actionBuffers)
-{
-    // Acties, size = 2
-    Vector3 controlSignal = Vector3.zero;
-    controlSignal.x = actionBuffers.ContinuousActions[0];
-    controlSignal.z = actionBuffers.ContinuousActions[1];
-    transform.Translate(controlSignal * speedMultiplier);
-
-    // Beloningen
-    float distanceToTarget = Vector3.Distance(this.transform.localPosition, Target.localPosition);
-
-    // target bereikt
-    if (distanceToTarget < 1.42f)
+    public override void OnActionReceived(ActionBuffers actionBuffers)
     {
-        SetReward(1.0f);
-        EndEpisode();
+        // Acties, size = 2
+        Vector3 controlSignal = Vector3.zero;
+        controlSignal.x = actionBuffers.ContinuousActions[0];
+        controlSignal.z = actionBuffers.ContinuousActions[1];
+        transform.Translate(controlSignal * speedMultiplier);
+        transform.Rotate(0.0f, rotationMultiplier * actionBuffers.ContinuousActions[1], 0.0f);
+
+        // Beloningen
+        float distanceToTarget = Vector3.Distance(this.transform.localPosition, Target.localPosition);
+
+        // target bereikt
+        if (distanceToTarget < 1.42f)
+        {
+            SetReward(1.0f);
+            EndEpisode();
+        }
+
+        // Van het platform gevallen?
+        else if (this.transform.localPosition.y < 0)
+        {
+            EndEpisode();
+        }
     }
 
-    // Van het platform gevallen?
-    else if (this.transform.localPosition.y < 0)
+    public override void Heuristic(in ActionBuffers actionsOut)
     {
-        EndEpisode();
+        var continuousActionsOut = actionsOut.ContinuousActions;
+        continuousActionsOut[0] = Input.GetAxis("Vertical");
+        continuousActionsOut[1] = Input.GetAxis("Horizontal");
     }
-}
 }
